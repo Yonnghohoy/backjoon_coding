44e8e1e6d46709b3046cbcb0705df7987fef27c6
yonnghohoy <akcnl5216@gmail.com>
2021-08-12 13:57:31 +0900

    bakcjoon

--- 4344.py
+++ 4344.py
@@ -1,1 +1,11 @@
-print("hello world")
+c = int(input())
+
+for start_case in range(c):
+    N = list(map(int,input().split()))
+    result = sum(N[1:])/N[0]
+    cnt=0
+    for score in N[1:]:
+        if score > result:
+            cnt +=1
+    rate=cnt/N[0]*100
+    print(f'{rate:.3f}%')
